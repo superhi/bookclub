421fcbac7552739922632841e0b18dc8f60b87f6
isabelxklee <hi@kleetime.com>
2021-05-17 11:45:43 -0400

    removed all filter features

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react'
 import BooksContainer from './components/BooksContainer'
 import Header from './components/Header'
 import DetailPanel from './components/DetailPanel'
-import Search from './components/Search'
 import {GlobalStyle} from './styles'
 import {Transition} from 'react-transition-group'
 
@@ -10,14 +9,12 @@ const App = () => {
   const [books, setBooks] = useState([])
   const [selectedBook, setSelectedBook] = useState(null)
   const [showPanel, setShowPanel] = useState(false)
-  const [filteredBooks, setFilteredBooks] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch('https://book-club-json.herokuapp.com/books')
       const books = await response.json()
       setBooks(books)
-      setFilteredBooks(books)
     }
 
     fetchData()
@@ -32,35 +29,11 @@ const App = () => {
     setShowPanel(false)
   }
 
-  const filterBooks = (searchTerm) => {
-    const stringSearch = (bookAttribute, searchTerm) =>
-      bookAttribute.toLowerCase().includes(searchTerm.toLowerCase())
-
-    if (!searchTerm) {
-      setFilteredBooks(books)
-    } else {
-      setFilteredBooks(
-        books.filter(
-          (book) => stringSearch(book.title, searchTerm) || stringSearch(book.author, searchTerm)
-        )
-      )
-    }
-  }
-
-  const hasFiltered = filteredBooks.length !== books.length
-
   return (
     <>
       <GlobalStyle />
-      <Header>
-        <Search filterBooks={filterBooks} />
-      </Header>
-      <BooksContainer
-        books={filteredBooks}
-        pickBook={pickBook}
-        isPanelOpen={showPanel}
-        title={hasFiltered ? 'Search results' : 'All books'}
-      />
+      <Header />
+      <BooksContainer books={books} pickBook={pickBook} isPanelOpen={showPanel} />
       <Transition in={showPanel} timeout={300}>
         {(state) => <DetailPanel book={selectedBook} closePanel={closePanel} state={state} />}
       </Transition>
--- src/components/BooksContainer/index.jsx
+++ src/components/BooksContainer/index.jsx
@@ -3,7 +3,7 @@ import {debounce} from 'lodash-es'
 import {Container, H2, BookList} from './styles'
 import Book from '../Book'
 
-const BooksContainer = ({books, pickBook, isPanelOpen, title}) => {
+const BooksContainer = ({books, pickBook, isPanelOpen}) => {
   const [scroll, setScroll] = useState(0)
   const prevPanelState = useRef(false)
 
@@ -27,11 +27,11 @@ const BooksContainer = ({books, pickBook, isPanelOpen, title}) => {
     }
 
     prevPanelState.current = isPanelOpen
-  }, [isPanelOpen, prevPanelState])
+  }, [isPanelOpen, prevPanelState, scroll])
 
   return (
     <Container $isPanelOpen={isPanelOpen} $top={scroll}>
-      <H2>{title}</H2>
+      <H2>All books</H2>
       <BookList>
         {books.map((book) => (
           <Book key={book.id} book={book} pickBook={pickBook} />
