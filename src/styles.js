06353157b04aa8c7956353b6df3c0da3f6f23817
isabelxklee <hi@kleetime.com>
2021-05-18 16:11:02 -0400

    added search feature and background color

--- src/styles.js
+++ src/styles.js
@@ -7,6 +7,7 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     -webkit-font-smoothing: antialiased;
     text-rendering: optimizeLegibility;
+    background-color: #a7e1f8;
   }
 `
 
